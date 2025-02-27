e7df39c02bf89ace1511e57b7f51506319502778
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-27 23:53:24 +0530

    create: useContent hook

---
+++ hooks/useContent.ts
@@ -0,0 +1,9 @@
+export default function useContent() {
+  return [
+    "https://i.pinimg.com/736x/02/98/5f/02985fee238c6d389ad115283f5d89a2.jpg",
+    "https://cdn.marvel.com/content/1x/snh_online_6072x9000_posed_01.jpg",
+    "https://creativereview.imgix.net/content/uploads/2024/12/AlienRomulus-scaled.jpg?auto=compress,format&q=60&w=729&h=1080",
+    "https://c8.alamy.com/comp/F6JN6R/release-date-april-22-2005-movie-title-kung-fu-hustle-studio-taihe-F6JN6R.jpg",
+    "https://www.vintagemovieposters.co.uk/wp-content/uploads/2021/01/IMG_1101-scaled.jpeg",
+  ];
+}
