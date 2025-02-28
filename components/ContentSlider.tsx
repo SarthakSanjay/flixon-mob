ab2ee4d28b97381d55c3b8a19e65db6b000c7270
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-02-28 23:24:43 +0530

    feat: add content slider component

---
+++ components/ContentSlider.tsx
@@ -0,0 +1,54 @@
+import useContent from "@/hooks/useContent";
+import { ScrollView, StyleSheet } from "react-native";
+import { ThemedView } from "./ThemedView";
+import { ThemedText } from "./ThemedText";
+import ContentCard from "./ContentCard";
+
+export default function ContentSlider({ genre }: { genre: string }) {
+  const content = useContent();
+  return (
+    <>
+      <ThemedText style={styles.sliderTitle}>{genre}</ThemedText>
+      <ScrollView horizontal={true}>
+        <ThemedView
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: 220,
+            width: "100%",
+          }}
+        >
+          <ThemedView style={styles.slider}>
+            {content.map((poster, index) => {
+              return <ContentCard key={index} poster={poster} />;
+            })}
+          </ThemedView>
+        </ThemedView>
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // borderWidth: 1,
+    borderColor: "green",
+  },
+  slider: {
+    height: "100%",
+    width: "100%",
+    display: "flex",
+    flexDirection: "row",
+    gap: 20,
+    justifyContent: "center",
+    alignItems: "center",
+    paddingInline: 10,
+  },
+  sliderTitle: {
+    height: 40,
+    verticalAlign: "bottom",
+    paddingInline: 10,
+    fontSize: 20,
+  },
+});
