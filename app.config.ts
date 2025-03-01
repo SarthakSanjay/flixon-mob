538e06132e7605f9a8995865a57a914c5a13c2ec
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-01 23:15:39 +0530

    add: app config to import baseUrl from env

---
+++ app.config.ts
@@ -0,0 +1,10 @@
+module.exports = {
+  expo: {
+    name: "netflix-mob",
+    extra: {
+      // apiUrl: process.env.API_URL || "https://default-api-url.com",
+      // apiKey: process.env.API_KEY || "default-development-key"
+      baseUrl: process.env.BASE_URL,
+    },
+  },
+};
