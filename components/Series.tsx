9a9ac10c7e0222957874a8c30b3438b740c7f6a4
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-01 23:26:47 +0530

    update: add prop type series

--- components/Series.tsx
+++ components/Series.tsx
@@ -1,5 +1,21 @@
 import { ThemedView } from "@/components/ThemedView";
+import { StyleSheet } from "react-native";
+import useGenre from "@/hooks/useGenre";
+import ContentSlider from "./ContentSlider";
 
 export default function Series() {
-  return <ThemedView>Series</ThemedView>;
+  const genres = useGenre();
+  return (
+    <ThemedView style={styles.container}>
+      {genres.map((genre, index) => {
+        return <ContentSlider key={index} type="series" genre={genre} />;
+      })}
+    </ThemedView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
