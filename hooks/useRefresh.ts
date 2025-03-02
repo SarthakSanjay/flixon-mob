8745e9890ff93dc3f29df2032f491c521fa37adb
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-02 23:38:23 +0530

    add: hook to refresh token

---
+++ hooks/useRefresh.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import * as SecureStore from "expo-secure-store";
+import Constants from "expo-constants";
+import { useRouter } from "expo-router";
+import { useState } from "react";
+const BASE_URL = Constants?.expoConfig?.extra?.baseUrl;
+export default function useRefresh() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+
+  const token = SecureStore.getItem("refresh_token");
+  const router = useRouter();
+  const refreshToken = async () => {
+    setLoading(true);
+    try {
+      const res = await axios.post(
+        `${BASE_URL}/api/refresh`,
+        {},
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        },
+      );
+      await SecureStore.setItemAsync("access_token", res.data.accessToken);
+      await SecureStore.setItemAsync("refresh_token", res.data.refreshToken);
+    } catch (error: any) {
+      if (error.response) {
+        router.replace({ pathname: "/(auth)/login" });
+      }
+      setError(error.response);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { refreshToken, loading, error };
+}
