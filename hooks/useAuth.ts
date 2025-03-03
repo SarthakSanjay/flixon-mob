4a3eb2495a4c31c15ddc2b94237130fe751d27b0
SARTHAK SANJAY <sarthaksanjaycoll21@gmail.com>
2025-03-03 23:33:25 +0530

    fix: useLogin hook ,add: useAuthCheck auth

--- hooks/useAuth.ts
+++ hooks/useAuth.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import * as SecureStore from "expo-secure-store";
 import Constants from "expo-constants";
+import { useRecoilState, useSetRecoilState } from "recoil";
+import { authState, userState } from "@/atoms/atom";
+import useRefresh from "./useRefresh";
 
 const BASE_URL = Constants?.expoConfig?.extra?.baseUrl;
 
 export function useLogin() {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
-
+  const [user, setUser] = useRecoilState(userState);
+  const setIsAuthenticated = useSetRecoilState(authState);
   const login = async (email: string, password: string) => {
     setLoading(true);
     setError(null);
@@ -23,14 +27,24 @@ export function useLogin() {
           withCredentials: true,
         },
       );
+      setUser({
+        Id: res.data?.user?._id,
+        Email: res.data?.user?.email,
+      });
 
-      await SecureStore.setItemAsync("access_token", res.data.accessToken);
-      await SecureStore.setItemAsync("refresh_token", res.data.refreshToken);
-      console.log(res.data);
+      if (res.status === 200) {
+        await SecureStore.setItemAsync("access_token", res.data.accessToken);
+        await SecureStore.setItemAsync("refresh_token", res.data.refreshToken);
+        await SecureStore.setItemAsync("userId", res.data.user._id);
+        setIsAuthenticated(true);
+      }
+
       return res.data;
-    } catch (error) {
-      setError("Login Failed please check credetials");
-      console.log("Login Failed", error);
+    } catch (error: any) {
+      if (error.response) {
+        setError("Login Failed please check credetials");
+        setIsAuthenticated(false);
+      }
     } finally {
       setLoading(false);
     }
@@ -59,4 +73,40 @@ export function useRegister() {
   };
 
   return { register, loading, error };
+}
+
+export function useAuthCheck() {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const [isAuthenticated, setIsAuthenticated] = useRecoilState(authState);
+  const { refreshToken } = useRefresh();
+
+  const checkAuth = async () => {
+    const access_token = await SecureStore.getItemAsync("access_token");
+    const userId = await SecureStore.getItemAsync("userId");
+    setLoading(true);
+    try {
+      if (access_token) {
+        const res = await axios.get(`${BASE_URL}/api/user/${userId}`, {
+          headers: {
+            Authorization: `Bearer ${access_token}`,
+          },
+        });
+
+        if (res.status === 200) {
+          setIsAuthenticated(true);
+        }
+      }
+    } catch (error: any) {
+      if (error.response && error.response.status === 401) {
+        await refreshToken(); // Ensure refreshToken is a function
+        setIsAuthenticated(false);
+      } else {
+        setError(error.message || "An error occurred");
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return { isAuthenticated, checkAuth, loading, error };
 }
